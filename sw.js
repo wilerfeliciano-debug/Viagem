eaac31faaf08baeb85769a2739088ffe4e262c17
wilerfeliciano-debug <wiler.feliciano@gmail.com>
2026-03-21 21:58:30 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
